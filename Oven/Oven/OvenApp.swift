28fc78e519a6a30f849c69e4ba3c494f52e40f25
금가경 <gageum0@gmail.com>
2023-05-06 08:30:43 +0900

    [Setting] : Create new file

---
+++ Oven/Oven/OvenApp.swift
@@ -0,0 +1,17 @@
+//
+//  OvenApp.swift
+//  Oven
+//
+//  Created by 금가경 on 2023/05/06.
+//
+
+import SwiftUI
+
+@main
+struct OvenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
