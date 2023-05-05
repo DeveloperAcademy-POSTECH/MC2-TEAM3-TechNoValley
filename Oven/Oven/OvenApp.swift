300fca9fd5da6c7a721db14349bc5b007b19a49f
금가경 <gageum0@gmail.com>
2023-05-06 08:39:50 +0900

    [Setting] : Create view structure

--- Oven/Oven/OvenApp.swift
+++ Oven/Oven/OvenApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct OvenApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                SplashView()
+            }
+            .navigationViewStyle(.stack)
         }
     }
 }
