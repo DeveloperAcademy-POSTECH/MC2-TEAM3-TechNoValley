ebec528f647f9a453261760bfc138479ad4f8ec4
금가경 <gageum0@gmail.com>
2023-05-06 20:28:59 +0900

    [Feat] Get user device angle

--- Oven/Oven/CompassHeading.swift
+++ Oven/Oven/CompassHeading.swift
@@ -15,26 +15,26 @@ class CompassHeading: NSObject, ObservableObject, CLLocationManagerDelegate {
             objectWillChange.send()
         }
     }
-
+    
     private let locationManager: CLLocationManager
-
+    
     override init() {
         self.locationManager = CLLocationManager()
         super.init()
-
+        
         self.locationManager.delegate = self
         self.setup()
     }
-
+    
     private func setup() {
         self.locationManager.requestWhenInUseAuthorization()
-
+        
         if CLLocationManager.headingAvailable() {
             self.locationManager.startUpdatingLocation()
             self.locationManager.startUpdatingHeading()
         }
     }
-
+    
     func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
         self.degrees = newHeading.magneticHeading
     }
