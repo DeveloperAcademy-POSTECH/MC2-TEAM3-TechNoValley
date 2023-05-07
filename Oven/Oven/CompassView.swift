802bd2548299d728cd2583123c02d0d9b5cd1210
금가경 <gageum0@gmail.com>
2023-05-07 15:11:39 +0900

    [Feat] Make compass moving

--- Oven/Oven/CompassView.swift
+++ Oven/Oven/CompassView.swift
@@ -9,10 +9,28 @@ import SwiftUI
 
 struct CompassView: View {
     @ObservedObject var compassHeading = CompassHeading()
-    @State private var randomAngle = Double.random(in: 0...360)
-
+    @State private var randomAngle = Double.random(in: -360...0)
+    
     var body: some View {
-            Text("\(compassHeading.degrees - randomAngle)")
+        let difference = compassHeading.degrees + randomAngle
+        GeometryReader { geometry in
+            let xCoordinate = difference/360 * geometry.size.width
+            
+            Path { path in
+                let x: CGFloat
+                switch xCoordinate {
+                case ..<0:
+                    x = xCoordinate + geometry.size.width
+                case 360...:
+                    x = xCoordinate - geometry.size.width
+                default:
+                    x = xCoordinate
+                }
+                path.move(to: CGPoint(x: x, y: geometry.size.height / 4))
+                path.addLine(to: CGPoint(x: x, y: geometry.size.height * 3 / 4))
+            }
+            .stroke(Color.orange)
+        }
     }
 }
 
