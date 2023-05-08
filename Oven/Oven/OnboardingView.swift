b8aab170df75461839fc4e74f5c6efde9dfe5e31
pscu91 <91787174+pscu91@users.noreply.github.com>
2023-05-08 17:26:15 +0900

    [Feat] onboarding slide view

--- Oven/Oven/OnboardingView.swift
+++ Oven/Oven/OnboardingView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    // 사용자 안내 온보딩 페이지를 앱 설치 후 최초 실행할 때만 띄우도록 하는 변수.
+    // @AppStorage에 저장되어 앱 종료 후에도 유지됨.
+    @AppStorage("_isFirstLaunching") var isFirstLaunching: Bool = true
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("App Main")
+        // 앱 최초 구동 시 전체화면으로 OnboardingTabView 띄우기
+        .fullScreenCover(isPresented: $isFirstLaunching) {
+            OnboardingTabView(isFirstLaunching: $isFirstLaunching)
+        }
     }
 }
 
