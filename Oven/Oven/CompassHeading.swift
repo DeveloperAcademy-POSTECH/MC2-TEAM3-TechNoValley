d453fe97326477998dfaa5f5b7abc48277929905
금가경 <gageum0@gmail.com>
2023-05-08 17:21:47 +0900

    [Fix] Modify CompassHeading branch

--- Oven/Oven/CompassHeading.swift
+++ Oven/Oven/CompassHeading.swift
@@ -4,7 +4,6 @@
 //
 //  Created by 금가경 on 2023/05/06.
 //
-
 import Foundation
 import Combine
 import CoreLocation
@@ -16,26 +15,26 @@ class CompassHeading: NSObject, ObservableObject, CLLocationManagerDelegate {
             objectWillChange.send()
         }
     }
-    
+
     private let locationManager: CLLocationManager
-    
+
     override init() {
         self.locationManager = CLLocationManager()
         super.init()
-        
+
         self.locationManager.delegate = self
         self.setup()
     }
-    
+
     private func setup() {
         self.locationManager.requestWhenInUseAuthorization()
-        
+
         if CLLocationManager.headingAvailable() {
             self.locationManager.startUpdatingLocation()
             self.locationManager.startUpdatingHeading()
         }
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
         self.degrees = newHeading.magneticHeading
     }
