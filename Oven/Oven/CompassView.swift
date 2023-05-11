435934ab88c009cc92fc30106db2690684390c42
금가경 <gageum0@gmail.com>
2023-05-11 16:01:52 +0900

    [Feat] Add font

--- Oven/Oven/CompassView.swift
+++ Oven/Oven/CompassView.swift
@@ -17,40 +17,46 @@ struct CompassView: View {
         NavigationView {
             let difference = compassHeading.degrees + randomAngle
             GeometryReader { geometry in
-                Path { path in
-                    var xCoordinate = difference/360 * geometry.size.width
-                    
-                    switch xCoordinate {
-                    case ..<0:
-                        xCoordinate = xCoordinate + geometry.size.width
-                    case 360...:
-                        xCoordinate = xCoordinate - geometry.size.width
-                    default:
-                        break
-                    }
-                    
-                    if !isMiddle {
-                        if Int(xCoordinate) == Int(geometry.size.width/2) {
-                            isMiddle.toggle()
-                            HapticManager.instance.notification(type: .success)
-                            HapticManager.instance.impact(style: .heavy)
-                            startTimer()
+                VStack {
+                    Text("이쪽이에요")
+                        .foregroundColor(Color(red: 1.00, green: 1.00, blue: 1.00))
+                        .font(.custom("esamanruOTFLight", size: 40))
+                    Path { path in
+                        var xCoordinate = difference/360 * geometry.size.width
+                        
+                        switch xCoordinate {
+                        case ..<0:
+                            xCoordinate = xCoordinate + geometry.size.width
+                        case 360...:
+                            xCoordinate = xCoordinate - geometry.size.width
+                        default:
+                            break
                         }
+                        
+                        if !isMiddle {
+                            if Int(xCoordinate) == Int(geometry.size.width/2) {
+                                isMiddle.toggle()
+                                HapticManager.instance.notification(type: .success)
+                                HapticManager.instance.impact(style: .heavy)
+                                startTimer()
+                            }
+                        }
+                        if isMiddle {
+                            xCoordinate = geometry.size.width/2
+                        }
+                        path.move(to: CGPoint(x: xCoordinate, y: geometry.size.height / 4))
+                        path.addLine(to: CGPoint(x: xCoordinate, y: geometry.size.height * 3 / 4))
                     }
-                    if isMiddle {
-                        xCoordinate = geometry.size.width/2
+                    .stroke(Color(red: 1.00, green: 0.74, blue: 0.00))
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color(red: 0.15, green: 0.15, blue: 0.15))
+                .background(
+                    NavigationLink(destination: TimerView(), isActive: $isShowingTimerView) {
                     }
-                    path.move(to: CGPoint(x: xCoordinate, y: geometry.size.height / 4))
-                    path.addLine(to: CGPoint(x: xCoordinate, y: geometry.size.height * 3 / 4))
+                )
+                .onAppear {
                 }
-                .stroke(Color.orange)
-            }
-            .background(
-                NavigationLink(destination: TimerView(), isActive: $isShowingTimerView) {
-                }
-            )
-            .onAppear {
-                isMiddle = false
             }
         }
     }
--- Oven/Oven/OvenApp.swift
+++ Oven/Oven/OvenApp.swift
@@ -9,10 +9,22 @@ import SwiftUI
 
 @main
 struct OvenApp: App {
+    init() {
+            Font.registerFonts()
+
+            //실제 폰트 이름 확인
+            //폰트 파일명과 실제 이름이 다를 수 있음
+            for family in UIFont.familyNames{
+                print(family)
+                for names in UIFont.fontNames(forFamilyName: family){
+                    print("== \(names)")
+                }
+            }
+        }
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                SplashView()
+                CompassView()
             }
             .navigationViewStyle(.stack)
         }
