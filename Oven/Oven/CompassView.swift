453fc2e7b5f737002dc9e66f2e795f82816f64d1
금가경 <gageum0@gmail.com>
2023-05-12 17:13:55 +0900

    [Design] Make Layout

--- Oven/Oven/CompassView.swift
+++ Oven/Oven/CompassView.swift
@@ -11,51 +11,106 @@ struct CompassView: View {
     @ObservedObject var compassHeading = CompassHeading()
     @State private var randomAngle = Double.random(in: -360...0)
     @State private var isMiddle = false
+    @State private var isLeft = false
+    @State private var isRight = false
     @State private var isShowingTimerView = false
     
     var body: some View {
         NavigationView {
             let difference = compassHeading.degrees + randomAngle
             GeometryReader { geometry in
-                VStack {
-                    Text("이쪽이에요")
-                        .foregroundColor(Color(red: 1.00, green: 1.00, blue: 1.00))
-                        .font(.custom("esamanruOTFLight", size: 40))
-                    Path { path in
-                        var xCoordinate = difference/360 * geometry.size.width
+                ZStack {
+                    VStack(spacing: geometry.size.height * 0.1) {
+                        Spacer()
                         
-                        switch xCoordinate {
-                        case ..<0:
-                            xCoordinate = xCoordinate + geometry.size.width
-                        case 360...:
-                            xCoordinate = xCoordinate - geometry.size.width
-                        default:
-                            break
-                        }
+                        Text(isMiddle ? "잘하셨어요!": "이쪽이에요")
+                            .foregroundColor(Color(red: 1.00, green: 1.00, blue: 1.00))
+                            .font(.custom("esamanruOTFLight", size: 21))
                         
-                        if !isMiddle {
-                            if Int(xCoordinate) == Int(geometry.size.width/2) {
-                                isMiddle.toggle()
-                                HapticManager.instance.notification(type: .success)
-                                HapticManager.instance.impact(style: .heavy)
-                                startTimer()
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color(red: 0.30, green: 0.30, blue: 0.30), lineWidth: 4)
+                                .frame(width: geometry.size.width * 0.3, height: geometry.size.width * 0.3)
+                            
+                            Path { path in
+                                path.move(to: CGPoint(x: geometry.size.width * 0.5, y: 0))
+                                path.addLine(to: CGPoint(x: geometry.size.width * 0.5, y: geometry.size.width * 0.3))
                             }
+                            .stroke(Color(red: 1.00, green: 1.00, blue: 1.00))
+                            .frame(height: geometry.size.width * 0.3)
+                            
+                            ZStack {
+                                if isRight {
+                                    Image(systemName: "arrowtriangle.left.fill")
+                                        .foregroundColor(Color(red: 1.00, green: 0.74, blue: 0.00))
+                                        .offset(x: -geometry.size.width * 0.22)
+                                }
+                                else if isLeft {
+                                    Image(systemName: "arrowtriangle.right.fill")
+                                        .foregroundColor(Color(red: 1.00, green: 0.74, blue: 0.00))
+                                        .offset(x: geometry.size.width * 0.22)
+                                }
+                                
+                                // 나침반 바늘
+                                Path { path in
+                                    var xCoordinate = difference/360 * geometry.size.width
+                                    
+                                    switch xCoordinate {
+                                    case ..<0:
+                                        xCoordinate = xCoordinate + geometry.size.width
+                                    case 360...:
+                                        xCoordinate = xCoordinate - geometry.size.width
+                                    default:
+                                        break
+                                    }
+                                    
+                                    if xCoordinate < geometry.size.width/2 {
+                                        isLeft = true
+                                        isRight = false
+                                    }
+                                    else if xCoordinate > geometry.size.width/2 {
+                                        isRight = true
+                                        isLeft = false
+                                    }
+                                    else {
+                                        isRight = false
+                                        isLeft = false
+                                    }
+                                    
+                                    if !isMiddle {
+                                        if Int(xCoordinate) == Int(geometry.size.width/2) {
+                                            isMiddle.toggle()
+                                            HapticManager.instance.notification(type: .success)
+                                            HapticManager.instance.impact(style: .heavy)
+                                            startTimer()
+                                        }
+                                    }
+                                    if isMiddle {
+                                        xCoordinate = geometry.size.width/2
+                                    }
+                                    path.move(to: CGPoint(x: xCoordinate, y: 0))
+                                    path.addLine(to: CGPoint(x: xCoordinate, y: geometry.size.width * 0.3))
+                                }
+                                .stroke(Color(red: 1.00, green: 0.74, blue: 0.00))
+                                .frame(height: geometry.size.width * 0.3)
+                            }
+                            .frame(height: geometry.size.width * 0.3)
                         }
-                        if isMiddle {
-                            xCoordinate = geometry.size.width/2
+                        Spacer()
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color(red: 0.15, green: 0.15, blue: 0.15))
+                    .background(
+                        NavigationLink(destination: TimerView(), isActive: $isShowingTimerView) {
                         }
-                        path.move(to: CGPoint(x: xCoordinate, y: geometry.size.height / 4))
-                        path.addLine(to: CGPoint(x: xCoordinate, y: geometry.size.height * 3 / 4))
+                    )
+                    if isMiddle {
+                        Text("이제 걸어볼까요?")
+                            .font(.custom("esamanruOTFLight", size: 14))
+                            .foregroundColor(Color(red: 1.00, green: 1.00, blue: 1.00))
+                            .offset(y: geometry.size.height * 0.15)
                     }
-                    .stroke(Color(red: 1.00, green: 0.74, blue: 0.00))
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color(red: 0.15, green: 0.15, blue: 0.15))
-                .background(
-                    NavigationLink(destination: TimerView(), isActive: $isShowingTimerView) {
-                    }
-                )
-                .onAppear {
                 }
             }
         }
