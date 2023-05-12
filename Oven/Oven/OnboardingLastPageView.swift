c7ab015c2c2444efc878ac69367bc55132a69a0b
pscu91 <91787174+pscu91@users.noreply.github.com>
2023-05-12 15:48:58 +0900

    [Fix] ZStack and Onbaording Layout

--- Oven/Oven/OnboardingLastPageView.swift
+++ Oven/Oven/OnboardingLastPageView.swift
@@ -16,36 +16,40 @@ struct OnboardingLastPageView: View {
 
     var body: some View {
         GeometryReader { geometry in
-            VStack {
-                Image(imageName)
-                    .frame(width: geometry.size.width, alignment: .center)
-                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 45, trailing: 0))
-
+            ZStack {
                 VStack {
-                    Text(title)
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .padding(.bottom)
-                    Text(subtitle)
-                        .padding(.bottom)
+                    Image(imageName)
+                        .frame(width: geometry.size.width, alignment: .center)
+                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 45, trailing: 0))
+
+                    VStack {
+                        Text(title)
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .padding(.bottom)
+                        Text(subtitle)
+                            .padding(.bottom)
+                            .fontWeight(.medium) //폰트의 두께
+
+                    }
+
+                    //                 온보딩 완료 버튼.
+                    //                 AppStorage의 isFirstLaunching 값을 false로 바꾸기 때문에, 다음번에 앱을 실행할 때는 OnboardingTabView를 띄우지 않음.
+                    Button {
+                        isFirstLaunching.toggle()
+                    } label: {
+                        Text("DOOR 시작하기")
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .frame(width: 200, height: 50)
+                            .background(Color(red: 1.00, green: 0.55, blue: 0.00))
+                            .cornerRadius(6)
+                    }
+                        .frame(width: geometry.size.width)
+                        .padding(EdgeInsets(top: 15, leading: 0, bottom: 100, trailing: 0))
                 }
-                    
-//                 온보딩 완료 버튼.
-//                 AppStorage의 isFirstLaunching 값을 false로 바꾸기 때문에, 다음번에 앱을 실행할 때는 OnboardingTabView를 띄우지 않음.
-                Button {
-                    isFirstLaunching.toggle()
-                } label: {
-                    Text("DOOR 시작하기")
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .frame(width: 200, height: 50)
-                        .background(Color(red: 1.00, green: 0.55, blue: 0.00))
-                        .cornerRadius(6)
-                }
-                    .frame(width: geometry.size.width)
-                    .padding(EdgeInsets(top: 15, leading: 0, bottom: 100, trailing: 0))
+                    .frame(width: geometry.size.width, height: geometry.size.height)
             }
-            .frame(width: geometry.size.width, height: geometry.size.height)
         }
     }
 }
--- Oven/Oven/OnboardingTabView.swift
+++ Oven/Oven/OnboardingTabView.swift
@@ -11,43 +11,47 @@ struct OnboardingTabView: View {
     @Binding var isFirstLaunching: Bool
 
     var body: some View {
-        TabView {
-            // 페이지
-            OnboardingPageView(
-                imageName: "guide-sample",
-                title: "열정적인 그대에게",
-                subtitle: "도어는 일상 속 작은 휴식을 선물합니다."
-            )
-
-            // 페이지
-            OnboardingPageView(
-                imageName: "guide-sample",
-                title: "도어는 간단해요.",
-                subtitle: "버튼만 누르면 오분 타이머가 시작돼요."
-            )
-
-            // 페이지
-            OnboardingPageView(
-                imageName: "guide-sample",
-                title: "시선을 돌려봐요.",
-                subtitle: "선을 화면 중앙에 맞춰 작은 여정을 시작해요."
-            )
-
-            // 페이지: 위치 권한 동의
-            OnboardingPermissionView(
-                title: "권한을 허용해주세요.",
-                subtitle: "도어는 휴식을 위한\n최소한의 권한만을 요구합니다."
-            )
-
-            // 페이지: 온보딩 완료
-            OnboardingLastPageView(
-                imageName: "check-circle",
-                title: "준비가 모두 끝났어요.",
-                subtitle: "바로 오분 간의 여정을 시작해보세요.",
-                isFirstLaunching: $isFirstLaunching
-            )
-        }
+        ZStack {
+            Color(red: 0.15, green: 0.15, blue: 0.15)
+                .ignoresSafeArea()
+            TabView {
+                // 페이지
+                OnboardingPageView(
+                    imageName: "guide-sample",
+                    title: "열정적인 그대에게",
+                    subtitle: "도어는 일상 속 작은 휴식을 선물합니다."
+                )
+                
+                // 페이지
+                OnboardingPageView(
+                    imageName: "guide-sample",
+                    title: "도어는 간단해요.",
+                    subtitle: "버튼만 누르면 오분 타이머가 시작돼요."
+                )
+                
+                // 페이지
+                OnboardingPageView(
+                    imageName: "guide-sample",
+                    title: "시선을 돌려봐요.",
+                    subtitle: "선을 화면 중앙에 맞춰 작은 여정을 시작해요."
+                )
+                
+                // 페이지: 위치 권한 동의
+                OnboardingPermissionView(
+                    title: "권한을 허용해주세요.",
+                    subtitle: "도어는 휴식을 위한\n최소한의 권한만을 요구합니다."
+                )
+                
+                // 페이지: 온보딩 완료
+                OnboardingLastPageView(
+                    imageName: "check-circle",
+                    title: "준비가 모두 끝났어요.",
+                    subtitle: "바로 오분 간의 여정을 시작해보세요.",
+                    isFirstLaunching: $isFirstLaunching
+                )
+            }
             .tabViewStyle(PageTabViewStyle())
-            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+//            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+        }
     }
 }
