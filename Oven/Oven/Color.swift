992ecb56cc9fdb559e91a44ec55bfa0cecdd2472
금가경 <gageum0@gmail.com>
2023-05-14 00:01:33 +0900

    [Add] Add color model

---
+++ Oven/Oven/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Oven
+//
+//  Created by 금가경 on 2023/05/13.
+//
+
+import Foundation
+import SwiftUI
+
+let surgeonOrange = Color(red: 1.00, green: 0.74, blue: 0.00)
+let chillBlack = Color(red: 0.15, green: 0.15, blue: 0.15)
+let Heavygray = Color(red: 0.30, green: 0.30, blue: 0.30)
+let LightGray = Color(red: 0.67, green: 0.67, blue: 0.67)
+let GlowWhite = Color(red: 1.00, green: 1.00, blue: 1.00)
