28807251bd7330ed966763f723ceace4a1cad290
금가경(Miya) <100858136+ge-um@users.noreply.github.com>
2023-08-24 21:09:47 +0900

    Revert "Door 1.0 ver"

--- Oven/Oven/OvenApp.swift
+++ Oven/Oven/OvenApp.swift
@@ -9,18 +9,6 @@ import SwiftUI
 
 @main
 struct OvenApp: App {
-    init() {
-            Font.registerFonts()
-
-            //실제 폰트 이름 확인
-            //폰트 파일명과 실제 이름이 다를 수 있음
-            for family in UIFont.familyNames{
-                print(family)
-                for names in UIFont.fontNames(forFamilyName: family){
-                    print("== \(names)")
-                }
-            }
-        }
     var body: some Scene {
         WindowGroup {
             NavigationView {
@@ -30,5 +18,3 @@ struct OvenApp: App {
         }
     }
 }
-
-
